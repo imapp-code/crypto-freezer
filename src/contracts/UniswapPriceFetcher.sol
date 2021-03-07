3ea26153b2c71b85f235bd59ebb6cc9fb81dffe4
rodiazet <radek.zagorowicz@gmail.com>
2021-03-07 15:26:36 +0100

    Price fetcher improvements and fixing.

--- src/contracts/UniswapPriceFetcher.sol
+++ src/contracts/UniswapPriceFetcher.sol
@@ -2,10 +2,12 @@ pragma solidity 0.7.6;
 
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "IPriceFetcher.sol";
 
 contract UniswapPriceFetcher is IPriceFetcher {
+    using SafeMath for uint256;
     IUniswapV2Factory private _uniswapFactory;
 
     uint8 constant public DECIMAL = 8;
@@ -20,12 +22,43 @@ contract UniswapPriceFetcher is IPriceFetcher {
         return DECIMAL;
     }
 
+    function _alignValue(uint256 value, uint8 decimals, uint8 targetDecimals) pure internal returns (uint256) {
+        return targetDecimals > decimals ?
+            value.mul((10**(targetDecimals - decimals))) :
+            value.div((10**(decimals - targetDecimals)));
+    }
+
+    function _getReserveAligned(IUniswapV2Pair pair) view internal returns (uint256, uint256) {
+        uint112 reserve0; uint112 reserve1;
+            (reserve0, reserve1,) = pair.getReserves();
+        ERC20 usdToken;
+        ERC20 token;
+        uint256 reserveUSD; uint256 reserveToken;
+        if (address(_usdStableContract) == pair.token0()) {
+            usdToken = ERC20(pair.token0());
+            token = ERC20(pair.token1());
+            reserveUSD = reserve0;
+            reserveToken = reserve1;
+        } else if (address(_usdStableContract) == pair.token1()) {
+            usdToken = ERC20(pair.token1());
+            token = ERC20(pair.token0());
+            reserveUSD = reserve1;
+            reserveToken = reserve0;
+        } else {
+            require(false, "No usd stable token in pair");
+        }
+
+        return (_alignValue(reserveUSD, usdToken.decimals(), decimals()),
+                _alignValue(reserveToken, token.decimals(), decimals()));
+    }
+
     function currentPrice(address tokenAddress) view external override returns (uint256) {
         IUniswapV2Pair pair = IUniswapV2Pair(_uniswapFactory.getPair(tokenAddress, address(_usdStableContract)));
+
         if(address(pair) != address(0)) {
-            uint112 reserve0; uint112 reserve1;
-            (reserve0, reserve1,) = pair.getReserves();
-            return (reserve1 * (10**decimals())) / reserve0;
+            uint256 reserveUSDAligned; uint256 reserveTokenAligned;
+            (reserveUSDAligned, reserveTokenAligned) = _getReserveAligned(pair);
+            return reserveUSDAligned.div(reserveTokenAligned);
         } else {
             return uint256(-1);
         }
