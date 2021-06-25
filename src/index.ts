ec5e86ae5861bfec6457223c18ea3a74e7f606f1
rodiazet <radek.zagorowicz@gmail.com>
2021-06-25 10:34:51 +0200

    fixing linter and packages.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {readFileSync} from 'fs'
-import {Contract, getDefaultProvider, Wallet} from 'ethers'
-import {CryptoFreezerFactory, TestErc20Factory, UniswapPriceFetcherFactory} from '../build'
+import {getDefaultProvider, Wallet} from 'ethers'
+import {CryptoFreezer__factory, TestERC20__factory, UniswapPriceFetcher__factory, CryptoFreezer} from '../build'
 
 async function run(args: string[]) {
     if (args.length !== 1) {
@@ -16,26 +16,26 @@ async function run(args: string[]) {
 
 async function deployContracts(deployer: Wallet) {
     console.log('Starting deployment of freezer contract:')
-    const factory = new CryptoFreezerFactory(deployer)
+    const factory = new CryptoFreezer__factory(deployer)
 
-    const factoryERC20 = new TestErc20Factory(deployer)
+    const factoryERC20 = new TestERC20__factory(deployer)
     const USDCAddress = await deployERC20Contract(factoryERC20, deployer, 'USD Coin', 'USDC', 6)
     const WETHAddress = await deployERC20Contract(factoryERC20, deployer, 'Wrapped Ether', 'WETH', 18)
     const wbtcAddress = await deployERC20Contract(factoryERC20, deployer, 'Wrapped BTC', 'WBTC', 8)
 
     const freezer = await deployFreezerContract(factory, wbtcAddress)
 
-    const factoryUniPF = new UniswapPriceFetcherFactory(deployer)
+    const factoryUniPF = new UniswapPriceFetcher__factory(deployer)
 
     const contractUniPF = await factoryUniPF.deploy('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', USDCAddress, WETHAddress)
     await contractUniPF.deployed()
     console.log(`Uni price fetcher implementation deployed at ${contractUniPF.address}`)
 
-    freezer.setPriceFetcher(contractUniPF.address)
+    await freezer.setPriceFetcher(contractUniPF.address)
 }
 
 async function deployERC20Contract(
-    factory: TestErc20Factory,
+    factory: TestERC20__factory,
     deployer: Wallet,
     name: string,
     symbol: string,
@@ -47,7 +47,7 @@ async function deployERC20Contract(
     return contract.address
 }
 
-async function deployFreezerContract(factory: CryptoFreezerFactory, wbtcAddress: string) : Promise<Contract> {
+async function deployFreezerContract(factory: CryptoFreezer__factory, wbtcAddress: string) : Promise<CryptoFreezer> {
     const contract = await factory.deploy()
     await contract.deployed()
     console.log(`CryptoFreezer implementation deployed at ${contract.address}`)
